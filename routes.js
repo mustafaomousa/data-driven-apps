f5ef7bda0732217037831ca6b126a1c69d7bbcce
Mustafa <mustafaomousa@icloud.com>
2020-11-19 14:54:20 -0600

    Queryed and rendered temp list of books

--- routes.js
+++ routes.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const db = require('./db/models');
 
-router.get('/', (req, res) => {
-    // throw new Error('This is a test error!');
-    res.render('index', { title: 'Home' });
+router.get('/', async (req, res, next) => {
+    try {
+        const books = await db.Book.findAll({ order: [['title', 'ASC']] });
+        res.render('index', { title: 'Home', books });
+    } catch (err) {
+        next(err);
+    }
 });
 
 module.exports = router;
